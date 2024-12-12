fe6f43100a9fcd35170c49700f137f524d17912f
GenmaHimmuro <t.suleimanov2@gmail.com>
2024-12-12 17:53:20 +0500

    API

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
 
 
-def main(place):
-    url = 'https://wttr.in/{}?lang=ru&M&n&q&T'.format(place)
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
+def main():
+    places = ['аэропорт Шереметьево', 'Лондон', "Череповец"]
+    payload = {'lang=ru':'','M':'','n':'','q':'','T':''}
+    for place in places:
+        url = 'https://wttr.in/{}'.format(place)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
 
 
 if __name__ == "__main__":
-    print(main('Лондон'), main('аэропорт Шереметьево'), main('Череповец'))
+    main()
